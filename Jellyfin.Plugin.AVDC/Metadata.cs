87122531e40074bece5a5d07e617fc807a7e6959
xjasonlyu <xjasonlyu@gmail.com>
2021-03-19 14:17:10 +0800

    chore: move ApiPath to Metadata.cs

--- Jellyfin.Plugin.AVDC/Metadata.cs
+++ Jellyfin.Plugin.AVDC/Metadata.cs
@@ -3,6 +3,15 @@ using System.Collections.Generic;
 
 namespace Jellyfin.Plugin.AVDC
 {
+    public static class ApiPath
+    {
+        public const string Metadata = "/metadata/";
+        public const string Actress = "/actress/";
+        public const string ActressImage = "/image/actress/";
+        public const string PrimaryImage = "/image/primary/";
+        public const string BackdropImage = "/image/backdrop/";
+    }
+
     public class Actress
     {
         public string Name { get; set; }
