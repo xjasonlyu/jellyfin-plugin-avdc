e650d76bea86d5c49d94aa56e4ca350b650ada8c
xjasonlyu <xjasonlyu@gmail.com>
2021-03-20 14:58:29 +0800

    add automatic job

---
+++ package.py
@@ -0,0 +1,39 @@
+import json
+import hashlib
+import sys
+from datetime import datetime
+
+
+def md5sum(filename):
+    with open(filename, 'rb') as f:
+        return hashlib.md5(f.read()).hexdigest()
+
+
+def generate(filename, version):
+    return {
+        "checksum": md5sum(filename),
+        "changelog": "Auto Released by Actions",
+        "targetAbi": "10.7.0.0",
+        "sourceUrl": "https://github.com/xjasonlyu/jellyfin-plugin-avdc/releases/download/"
+                     f"v{version}/Jellyfin.Plugin.AVDC.zip",
+        "timestamp": datetime.now().strftime("%Y-%m-%dT%H:%M:%SZ"),
+        "version": version
+    }
+
+
+def main():
+    filename, version = sys.argv[1:]
+
+    with open('manifest.json') as f:
+        manifest = json.load(f)
+
+    manifest[0]['versions'].insert(
+        0,
+        generate(filename, version)
+    )
+
+    print(json.dumps(manifest, indent=2))
+
+
+if __name__ == '__main__':
+    main()
