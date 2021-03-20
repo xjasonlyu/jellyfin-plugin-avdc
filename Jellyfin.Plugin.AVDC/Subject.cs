d4a43daa9f8efe848a4af04b2db0463cf0c36433
xjasonlyu <xjasonlyu@gmail.com>
2021-03-20 10:51:21 +0800

    chore: rename

--- Jellyfin.Plugin.AVDC/Subject.cs
+++ Jellyfin.Plugin.AVDC/Subject.cs
@@ -5,8 +5,8 @@ namespace Jellyfin.Plugin.AVDC
 {
     public static class ApiPath
     {
-        public const string Metadata = "/metadata/";
         public const string Actress = "/actress/";
+        public const string Metadata = "/metadata/";
         public const string ActressImage = "/image/actress/";
         public const string PrimaryImage = "/image/primary/";
         public const string BackdropImage = "/image/backdrop/";
