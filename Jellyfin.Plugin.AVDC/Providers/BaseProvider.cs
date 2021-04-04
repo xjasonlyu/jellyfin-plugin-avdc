3a4459d4d6aa14eb6f4b528a3c6053bdb8eb0479
xjasonlyu <xjasonlyu@gmail.com>
2021-04-04 21:50:06 +0800

    optimize SetProviderIds

--- Jellyfin.Plugin.AVDC/Providers/BaseProvider.cs
+++ Jellyfin.Plugin.AVDC/Providers/BaseProvider.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Linq;
+using System.Reflection;
 using System.Threading;
 using System.Threading.Tasks;
 using MediaBrowser.Model.Entities;
@@ -51,10 +53,17 @@ namespace Jellyfin.Plugin.AVDC.Providers
             return name; // do nothing
         }
 
+        private static string[] ProviderNames => typeof(Constants).GetFields(BindingFlags.Public | BindingFlags.Static)
+            .Select(x => x.GetValue(null).ToString()).ToArray();
+
         protected static void SetProviderIds(IHasProviderIds item, string[] providers, string[] sources)
         {
             for (var i = 0; i < Math.Min(providers.Length, sources.Length); i++)
-                item.SetProviderId(providers[i], sources[i]);
+            {
+                var name = Array.Find(ProviderNames, e => e.Equals(providers[i], StringComparison.OrdinalIgnoreCase));
+                if (!string.IsNullOrEmpty(name) && !name.Equals(Constants.Avdc))
+                    item.SetProviderId(name, sources[i]);
+            }
         }
     }
 }
