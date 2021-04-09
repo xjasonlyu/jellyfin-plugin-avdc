5a41dbcf29350d69e30cfb88975dfb2d4b7ed960
xjasonlyu <xjasonlyu@gmail.com>
2021-04-09 16:40:44 +0800

    support new provider: arzon

--- Jellyfin.Plugin.AVDC/Constant.cs
+++ Jellyfin.Plugin.AVDC/Constant.cs
@@ -3,6 +3,7 @@ namespace Jellyfin.Plugin.AVDC
     public static class Constant
     {
         public const string Avdc = "AVDC";
+        public const string Arzon = "ARZON";
         public const string Avsox = "AVSOX";
         public const string Fanza = "FANZA";
         public const string Fc2 = "FC2";
---
+++ Jellyfin.Plugin.AVDC/ExternalIds/ArzonExternalId.cs
@@ -0,0 +1,32 @@
+using MediaBrowser.Controller.Entities.Movies;
+using MediaBrowser.Controller.Providers;
+using MediaBrowser.Model.Entities;
+
+#if !__EMBY__
+using MediaBrowser.Model.Providers;
+
+#endif
+
+namespace Jellyfin.Plugin.AVDC.ExternalIds
+{
+    public class ArzonExternalId : IExternalId
+    {
+#if __EMBY__
+        public string Name => Constant.Arzon;
+#else
+        public string ProviderName => Constant.Arzon;
+#endif
+        public string Key => Constant.Arzon;
+
+        public string UrlFormatString => "{0}";
+
+#if !__EMBY__
+        public ExternalIdMediaType? Type => ExternalIdMediaType.Movie;
+#endif
+
+        public bool Supports(IHasProviderIds item)
+        {
+            return item is Movie;
+        }
+    }
+}
